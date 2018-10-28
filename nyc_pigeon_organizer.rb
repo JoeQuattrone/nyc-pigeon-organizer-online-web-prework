3847cd12e6169be67447afa5ccd86f3d82e3b3bb
Joe Quattrone <joequattrone5@gmail.com>
2018-10-28 02:32:56 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,5 @@
 require 'pry'
+<<<<<<< HEAD
 =begin
 def nyc_pigeon_organizer(data)
   new_hash = {}
@@ -9,10 +10,28 @@ def nyc_pigeon_organizer(data)
         new_hash[name] ||= {}
         new_hash[name][attributes] ||= []
         new_hash[name][attributes].push(key.to_s)
+=======
+
+def nyc_pigeon_organizer(data)
+  new_hash = {}
+=begin  
+  data[:lives].each do |city, name|
+    name.each do |i|
+    new_hash[i] = data.each do |key, sub_key|
+      sub_key.each do |attribute, value|
+        value.each do |array_name|
+          if array_name.include?(i)
+            "cool"  
+
+            end
+          end
+        end
+>>>>>>> a3d59c4639c953a0147d00e33a5badbad5a41b86
       end
     end
   end
   new_hash
+<<<<<<< HEAD
   binding.pry
 end
 =end
@@ -27,8 +46,22 @@ def nyc_pigeon_organizer(data)
         new_hash[name] ||= {}
         new_hash[name][attribute] ||= []
         new_hash[name][attribute].push("#{key}")
+=======
+=end                    
+
+  data.each do |attributes, values|
+    values.each do |key, names|
+      names.each do |name|
+        new_hash[name] ||= {}
+        new_hash[name][attributes] ||= []
+        new_hash[name][attrubutes].push(key.to_s)
+>>>>>>> a3d59c4639c953a0147d00e33a5badbad5a41b86
       end
     end
   end
   new_hash
+<<<<<<< HEAD
 end
+=======
+end
+>>>>>>> a3d59c4639c953a0147d00e33a5badbad5a41b86
